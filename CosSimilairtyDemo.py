88ba02171a0c72aaee549f9915f26937c4861c6f
Liu Jun <dummygsy@hotmail.com>
2018-02-23 10:49:48 +0800

    Update CosSimilairtyDemo.py

--- CosSimilairtyDemo.py
+++ CosSimilairtyDemo.py
@@ -8,18 +8,70 @@ nltk.download('punkt') # if necessary...
 stemmer = nltk.stem.porter.PorterStemmer()
 remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
 
+# Split the sentence into words and get the stem of each word by Porter Stemmer Algorithm
+#
+# >>> stemmer.stem("creates")
+# 'creat'
+# >>> stemmer.stem("created")
+# 'creat'
+# >>> stemmer.stem("create")
+# 'creat'
+#
+# >>> stem_tokens(nltk.word_tokenize("I love a little bird!!"))
+# ['I', 'love', 'a', 'littl', 'bird', '!', '!']
+#
 def stem_tokens(tokens):
     return [stemmer.stem(item) for item in tokens]
 
 # Remove punctuation, lowercase, stem
+# 
+# Split the sentence into word tokens
+# >>> nltk.word_tokenize("I love a little bird!!")
+# ['I', 'love', 'a', 'little', 'bird', '!', '!']
+#
+# lower() become uppercase into lowercase (I become into i)
+# >>> nltk.word_tokenize("I love a little bird!!".lower())
+# ['i', 'love', 'a', 'little', 'bird', '!', '!']
+#
+# translate(remove_punctuation_map) remove punctuation (remove !)
+# >>> nltk.word_tokenize("I love a little bird!!".lower().translate(remove_punctuation_map))
+# ['i', 'love', 'a', 'little', 'bird']
+#
 def normalize(text):
     return stem_tokens(nltk.word_tokenize(text.lower().translate(remove_punctuation_map)))
 
 vectorizer = TfidfVectorizer(tokenizer=normalize, stop_words='english')
 
 def cosine_sim(text1, text2):
-    tfidf = vectorizer.fit_transform([text1, text2])
-    return ((tfidf * tfidf.T).A)[0,1]
+    # Learn vocabulary and idf, return term-document matrix. This is equivalent to fit followed by transform, but more efficiently implemented.
+	tfidf = vectorizer.fit_transform([text1, text2]) 
+	return ((tfidf * tfidf.T).A)[0,1]
+
+
+# >>> tfidf = vectorizer.fit_transform(['a little bird', 'little dog'])
+# >>> tfidf
+# <2x3 sparse matrix of type '<class 'numpy.float64'>'
+#        with 4 stored elements in Compressed Sparse Row format>
+# >>> print(tfidf)
+#   (0, 2)        0.5797386715376657
+#   (0, 0)        0.8148024746671689
+#   (1, 2)        0.5797386715376657
+#   (1, 1)        0.8148024746671689
+# >>> print(tfidf.T)
+#   (2, 0)        0.5797386715376657
+#   (0, 0)        0.8148024746671689
+#   (2, 1)        0.5797386715376657
+#   (1, 1)        0.8148024746671689
+# >>> print(tfidf * tfidf.T)
+#   (0, 1)        0.3360969272762574
+#   (0, 0)        0.9999999999999999
+#   (1, 1)        0.9999999999999999
+#   (1, 0)        0.3360969272762574
+# >>> print((tfidf * tfidf.T).A)
+# [[1.         0.33609693]
+#  [0.33609693 1.        ]]
+# >>> print(((tfidf * tfidf.T).A)[0,1])
+# 0.3360969272762574	
 
 
 print(cosine_sim('a little bird', 'a little bird'))
