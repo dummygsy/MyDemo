f837eeac12f0df38fce7ad01525790905049765e
Liu Jun <dummygsy@hotmail.com>
2018-03-06 17:21:36 +0800

    Count Word Frequence

--- NLTKDemo.py
+++ NLTKDemo.py
@@ -1,15 +1,32 @@
 import urllib.request
 from bs4 import BeautifulSoup
- 
-response = urllib.request.urlopen('http://php.net/')
- 
-html = response.read()
-print ("html file:")
-print (html)
+import nltk
 
+''' Download the webpage '''
+response = urllib.request.urlopen('http://php.net/')
+
+''' Get the HTML raw data '''
+html = response.read()
+#print ("html file:")
+#print (html)
+
+''' Get the processed clean text '''
 soup = BeautifulSoup(html)
-print ("soup:")
-print (soup)
+#print ("soup:")
+#print (soup)
 text = soup.get_text(strip=True)
-print ("processed text:")
-print (text)
+#print ("processed text:")
+#print (text)
+
+''' Split text to tokens '''
+tokens = [t for t in text.split()]
+#print ("tokens:")
+#print (tokens)
+
+
+''' Count Word Frequence '''
+freq = nltk.FreqDist(tokens)
+for key,val in freq.items():
+    print (str(key) + ':' + str(val))
+
+freq.plot(20, cumulative=False)
