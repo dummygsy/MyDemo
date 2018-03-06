edfb01d0073099bd40999c37217f59aa0844c1bd
Liu Jun <dummygsy@hotmail.com>
2018-03-06 17:09:47 +0800

    print the clean text

--- NLTKDemo.py
+++ NLTKDemo.py
@@ -1,7 +1,15 @@
 import urllib.request
+from bs4 import BeautifulSoup
  
 response = urllib.request.urlopen('http://php.net/')
  
 html = response.read()
- 
+print ("html file:")
 print (html)
+
+soup = BeautifulSoup(html)
+print ("soup:")
+print (soup)
+text = soup.get_text(strip=True)
+print ("processed text:")
+print (text)
